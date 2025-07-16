3b18bb5f934c3336b5e1b217afb032a370cf13aa
susmisc14 <su.smisc14@gmail.com>
2025-07-17 00:32:19 +0900

    feat: renderElement.js 파일을 TypeScript 파일로 변환 및 DOM 렌더링 로직 구현

--- src/lib/renderElement.ts
+++ src/lib/renderElement.ts
@@ -1,10 +1,18 @@
-import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
+import { setupEventListeners } from "./eventManager";
 import { normalizeVNode } from "./normalizeVNode";
+import type { VNode } from "./types";
 import { updateElement } from "./updateElement";
 
-export function renderElement(vNode, container) {
+export function renderElement(vNode: VNode, container: HTMLElement) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+
+  const newNode = normalizeVNode(vNode);
+
+  container.innerHTML = "";
+  container.appendChild(createElement(newNode));
+
+  setupEventListeners(container);
 }
