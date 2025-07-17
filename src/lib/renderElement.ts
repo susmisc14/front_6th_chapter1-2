cab81faefd841f4f65f803bf3b0c9ca538660071
susmisc14 <su.smisc14@gmail.com>
2025-07-17 15:47:11 +0900

    feat: renderElement 함수에 기존 DOM 업데이트 로직 추가 및 oldNode 상태 관리

--- src/lib/renderElement.ts
+++ src/lib/renderElement.ts
@@ -4,6 +4,8 @@ import { normalizeVNode } from "./normalizeVNode";
 import type { VNode } from "./types";
 import { updateElement } from "./updateElement";
 
+let oldNode: VNode = null;
+
 export function renderElement(vNode: VNode, container: HTMLElement) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
@@ -11,8 +13,13 @@ export function renderElement(vNode: VNode, container: HTMLElement) {
 
   const newNode = normalizeVNode(vNode);
 
-  container.innerHTML = "";
-  container.appendChild(createElement(newNode));
+  if (container.firstChild) {
+    updateElement(container, newNode, oldNode);
+  } else {
+    container.appendChild(createElement(newNode));
+  }
+
+  oldNode = newNode;
 
   setupEventListeners(container);
 }
